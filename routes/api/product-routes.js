0730f92e5e8bd923947f86bd33a3f440a94768a4
Kyle <kylemphillips65@yahoo.com>
2023-01-25 14:23:32 -0500

    added more to product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const products = await Product.findAll({
       include: [{
@@ -15,6 +15,56 @@ router.get('/', (req, res) => {
     res.json(err);
   }
 });
+
+
+router.get('/:id', async (req, res) => {
+  try {
+    const products = await Product.findByPk(req.params.id, {
+      include: [{
+        model: Category,
+        model: Tag,
+      }]
+    });
+    res.json(products);
+  } catch (err) {
+    console.error(err);
+    res.json(err);
+  }
+});
+
+// create new product
+  /* req.body should look like this...
+    {
+      product_name: "Jordan Shoes",
+      price: 300.00,
+      stock: 2,
+      category_id: 
+      tagIds: [1, 2, 3, 4]
+    }
+  */
+
+    
+    router.post('/', (req, res) => {
+  Product.create(req.body)
+    .then((product) => {
+      if (req.body.tagIds.length) {
+        const productsTag = req.body.tagIds.map((tag_id) => {
+          return {
+            product_id: product.id,
+            tag_id,
+          };
+        });
+        return ProductTag.bulkCreate(productsTag);
+      }
+      res.status(200).json(product);
+    })
+    .then((productsTagIds) => res.status(200).json(productsTagIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
 
 
 
