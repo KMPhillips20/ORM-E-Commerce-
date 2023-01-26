db4dca195d4d60e5d8e2f43a49a410eb04f630e5
Kyle <kylemphillips65@yahoo.com>
2023-01-26 15:38:13 -0500

    added to product js

--- models/Tag.js
+++ models/Tag.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
   {
@@ -12,9 +12,9 @@ Tag.init(
       primaryKey: true,
       autoIncrement: true,
     },
-      tag_name: {
-        type: DataTypes.STRING,
-      }
+    tag_name: {
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
